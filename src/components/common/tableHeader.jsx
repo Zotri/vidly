0db416cc6a09b297e1920891c02b77af0bb4947b
motri <mahmoud.otri@senacor.com>
2020-11-02 10:27:26 +0100

    add css detail
    Signed-off-by: motri <mahmoud.otri@senacor.com>

--- src/components/common/tableHeader.jsx
+++ src/components/common/tableHeader.jsx
@@ -14,15 +14,22 @@ class TableHeader extends Component {
 		// raise the sort event
 		this.props.onSort(sortColumn);
 	};
+	renderColumnSort = (column) => {
+		const { sortColumn } = this.props;
+		if (column.path !== sortColumn.path) return;
+		if (sortColumn.order === "asc") return <i className='fa fa-sort-asc'></i>;
+		return <i className='fa fa-sort-desc'></i>;
+	};
 	render() {
 		return (
 			<thead>
 				<tr>
 					{this.props.columns.map((column) => (
 						<th
+							className='clickable'
 							key={column.path || column.key}
 							onClick={() => this.raiseSort(column.path)}>
-							{column.lable}
+							{column.lable} {this.renderColumnSort(column)}
 						</th>
 					))}
 				</tr>
